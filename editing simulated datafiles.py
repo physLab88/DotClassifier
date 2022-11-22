d81ee46724e5986fb0b43bc2ec4154fa629200c2
Michael Bedard <88888888mic@gmail.com>
2022-11-22 10:54:40 -0500

    cropable datafiles

--- editing simulated datafiles.py
+++ editing simulated datafiles.py
@@ -4,9 +4,9 @@ ex: remove certain files from the dataset'''
 import numpy as np
 import yaml
 import os
+import matplotlib.pyplot as plt
 
-
-root_dir = "data/sim3_0/valid/"
+root_dir = "data/exp_croped/"
 
 
 def verify_if_files_attached(delete_unattached=False):
@@ -77,9 +77,70 @@ def verify_min_img_size(min_size=35, del_too_small=False):
     return
 
 
+def crop_image():
+    """ This function is used to manually crop images. mostly used to crop experimental data"""
+    f = open(root_dir + '_data_indexer.yaml', 'r')
+    infos = yaml.load(f, Loader=yaml.FullLoader)
+    print(len(infos))
+    for index in range(len(infos)):
+        info = infos[index]
+        sample_name = root_dir + info['f'] + '.npy'
+        diagram = np.load(sample_name)
+
+        UL = [0, 0]
+        LR = [-1, -1]
+        while True:
+            plt.imshow(np.log(np.abs(diagram[UL[1]:LR[1], UL[0]:LR[0]])), cmap='hot')
+            plt.show()
+            is_good = input("Is the diagram good? (y/n/c):  ")
+            if is_good == 'y':
+                Vg = np.linspace(info['Vg_range'][0], info['Vg_range'][1], info['nVg'])
+                Vds = np.linspace(info['Vds_range'][0], info['Vds_range'][1], info['nVds'])
+                Vg = np.concatenate([Vg, [0]])[UL[0]: LR[0]]
+                Vds = np.concatenate([Vds, [0]])[UL[1]: LR[1]]
+                if len(Vg) < 33:
+                    print('error, Vg does not have enough pixels')
+                elif len(Vds) < 33:
+                    print('error, Vds does not have enough pixels')
+                else:
+                    info['Vg_range'] = [float(Vg[0]), float(Vg[-1])]
+                    info['nVg'] = len(Vg)
+                    info['Vds_range'] = [float(Vds[0]), float(Vds[-1])]
+                    info['nVds'] = len(Vds)
+                    diagram = diagram[UL[1]:LR[1], UL[0]:LR[0]]
+                    # save everything
+                    np.save(sample_name, diagram)
+                    f = open(root_dir + '_data_indexer.yaml', 'w')
+                    yaml.dump(infos, f)
+                    print('yay')
+                    break
+            elif is_good == 'c':
+                # c is for cancel, won't save or change anything of this datapoint
+                break
+            uper_left = input("uper left corner coords:  ")
+            uper_left = uper_left.split()
+            UL = [0, 0]
+            for i, coord in zip(range(2), uper_left):
+                if coord != '-':
+                    print('changing coord')
+                    UL[i] = int(coord)
+
+            lower_right = input("lower right corner coords: ")
+            lower_right = lower_right.split()
+            LR = [-1, -1]
+            for i, coord in zip(range(2), lower_right):
+                if coord != '-':
+                    print('changing coord')
+                    LR[i] = int(coord)
+            print(UL)
+            print(LR)
+    return
+
+
 def main():
     # verify_if_files_attached(delete_unattached=False)
-    verify_min_img_size(del_too_small=False)
+    # verify_min_img_size(del_too_small=False)
+    crop_image()
 
 
 if __name__ == '__main__':
